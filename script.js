70e5dd1033c6b0f2265e8d37cf9fe907b57d9df0
Emma Franzén <emmasmejladress@gmail.com>
2026-08-03 10:44:52 +0200

    Change day check from Wednesday to Tuesday

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Visa bara fredag
-  if (new Date().getDay() !== 3) {
+  if (new Date().getDay() !== 2) {
     quizBox.innerHTML = "<p>Kom tillbaka på tisdag för fler lill-fredagsfrågor! 📅</p>";
   } else {
     hamtaVeckansFragor();
